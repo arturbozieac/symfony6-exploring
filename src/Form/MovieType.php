b3e71700d5a82cc3e9776207f6b7fdbf529e76b0
Artur Bozieac <barty1215@gmail.com>
2024-01-09 10:17:59 +0200

    add dto & forms

---
+++ src/Form/MovieType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Movie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('poster', TextType::class)
+            ->add('country', TextType::class)
+            ->add('releasedAt', DateType::class, [
+                'input' => 'datetime_immutable',
+                'widget' => 'single_text',
+            ])
+            ->add('plot', TextareaType::class)
+            ->add('price', MoneyType::class, [
+                'required' => false,
+                'scale' => 100,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
